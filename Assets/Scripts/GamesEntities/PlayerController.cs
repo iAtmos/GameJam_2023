26418c49aa107ba965f28f61f108bc38b62422f8
iAtmos <azeev.b01@gmail.com>
2023-07-10 12:21:23 +0500

    Модельки/Ходьба
    - Добавлена возможность поворота игрока
    - Пакт моделек скальных пород
    - Первоначальная текстура поверхности (требует явной доработки)

--- Assets/Scripts/GamesEntities/PlayerController.cs
+++ Assets/Scripts/GamesEntities/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : AbstractEntity
     private const float correctorSpeed = 10000f;
     private float horizontalX;
     private float horizontalZ;
+    private float rotationY;
+
+    [Header("Miscalculation movement")]
+    [field: SerializeField] private Camera viewPlayer;
     private Vector3 moveDerection;
 
     [Header("—haracter Parameters")]
@@ -27,6 +31,7 @@ public class PlayerController : AbstractEntity
     protected void Update()
     {
         PlayerInput();
+        PlayerOffsetAngle();
         animator.SetFloat(SpeedWalk, ConversionRange(rb.velocity.magnitude, MaxSpeedMove));
     }
 
@@ -51,6 +56,13 @@ public class PlayerController : AbstractEntity
             moveDerection = Vector3.zero;
         }
 
+        transform.rotation = Quaternion.Euler(0f, rotationY, 0f); // !!! »«Ã≈Õ»“‹ Õ¿ ‘»«» ” !!!
         rb.AddRelativeForce(moveDerection.normalized * correctorSpeed * Time.fixedDeltaTime, ForceMode.Force);
+    }
+
+    private void PlayerOffsetAngle()
+    {
+        var differenceBetweenCursorAndPlayerPosition = viewPlayer.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        rotationY = Mathf.Atan2(differenceBetweenCursorAndPlayerPosition.x, differenceBetweenCursorAndPlayerPosition.z) * Mathf.Rad2Deg;
     }
 }
