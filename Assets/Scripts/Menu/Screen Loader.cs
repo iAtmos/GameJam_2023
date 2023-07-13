6bb2353b295cb7675edaaeb9774b9f39b244fae6
Owline <Mikhail.Zavorokhin@urfu.me>
2023-07-14 03:21:23 +0500

    Screen Loader

---
+++ Assets/Scripts/Menu/Screen Loader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenLoader : MonoBehaviour
+{
+    public void SceneLoad(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
